e3fe6844a5e163f5d92d31c8e0cc849192de29c2
Juan Dario Rodas M. <jdrodas@hotmail.com>
2025-03-09 08:57:43 -0500

    Actualizado proyecto ListaEnterosAleatorios, agregada funcionalidades de ordenamiento ascendente y descendente

--- ListaEnterosAleatorios/main.py
+++ ListaEnterosAleatorios/main.py
@@ -16,6 +16,7 @@
 
 import random
 
+#Visualiza la lista de manera tabulada, 10 valores por línea
 def visualizar_lista(la_lista):
     for posicion in range(0, len(la_lista)):
         if (posicion+1) % 10 ==0:
@@ -23,6 +24,7 @@ def visualizar_lista(la_lista):
         else:
             print(la_lista[posicion],'\t', end="")
 
+# Obtiene una lista de numeros pares incluida en la lista parámetro
 def obtener_lista_pares(la_lista):
     lista_numeros_pares = []
     for i in range(0,len(la_lista)):
@@ -31,6 +33,7 @@ def obtener_lista_pares(la_lista):
 
     return lista_numeros_pares
 
+# Obtiene una lista de numeros impares incluida en la lista parámetro
 def obtener_lista_impares(la_lista):
     lista_numeros_impares = []
     for i in range(0,len(la_lista)):
@@ -40,7 +43,7 @@ def obtener_lista_impares(la_lista):
     return lista_numeros_impares
 
 # Aqui es la función principal
-print('Programa para generar una lista de 100 números enteros aleatorios')
+print('Programa para generar una lista de 100 números enteros aleatorios \n')
 
 #Aqui generamos la lista
 la_lista = []
@@ -53,10 +56,24 @@ visualizar_lista(la_lista)
 
 #Aqui encontramos la sublista de los numeros pares
 lista_pares = obtener_lista_pares(la_lista)
-print(f'\nLa lista con los números pares es:')
+print(f'\n\nLa lista con los números pares es:')
 visualizar_lista(lista_pares)
 
 #Aquí encontramos la sublista de los numeros impares
 lista_impares = obtener_lista_impares(la_lista)
-print(f'\nLa lista con los números impares es:')
+print(f'\n\nLa lista con los números impares es:')
 visualizar_lista(lista_impares)
+
+print(f'\n\nLa lista con los números impares, ordenada ascendentemente es:')
+#Ordenar con sort() altera el orden de los elementos de la lista
+lista_impares.sort()
+visualizar_lista(lista_impares)
+
+print(f'\n\nLa lista con los números pares, ordenada descendentemente es:')
+#Ordenar con sorted() genera una copia de la lista sin afectar la original
+lista_pares_descendentes = sorted(lista_pares, reverse=True)
+visualizar_lista(lista_pares_descendentes)
+
+print(f'\n\nLa lista con los números pares original es:')
+visualizar_lista(lista_pares)
+
